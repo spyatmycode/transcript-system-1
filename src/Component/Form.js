517ff5d5bff186ea3bc85af01d82db96c441fc9a
spyatmycode <akejunifemi11@gmail.com>
2023-01-20 00:51:34 +0100

    Update: auth admin

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -9,12 +9,12 @@
 //                 <button  className='btn btn-dark '>
 //                 <BsChevronLeft className='' size={30}/> back
 //                 </button>
-                          
-                
-          
+
+
+
 //         </div>
 //         <Header/>
-       
+
 
 
 
@@ -22,7 +22,7 @@
 //        <form className=''>
 
 //                 <div className="d-flex flex-column   container align-items-center justify-content-center">
-                        
+
 
 //                         <div className="tilte h1 text-danger fw-semibold my-5">
 //                         <h1>STUDENT FORM</h1> 
@@ -30,7 +30,7 @@
 
 
 //                     <div className='gird  gap-2'>
-                     
+
 //                     <div class="mb-3 g-col-6 g-col-md-4">
 //                             <label class="form-label">Name (Nom)</label>
 //                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="e.g Adewale Philips"/>
@@ -39,7 +39,7 @@
 //                             <label class="form-label">Matric No (No Matricule): </label>
 //                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="e.g TUMST/**/*****/***/*****"/>
 //                     </div>
-                        
+
 //                     <div class="mb-3 g-col-6 g-col-md-4">
 //                             <label  class="form-label">College</label>
 //                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="e.g SOCIAL & MANAGEMENT SCIENCE"/>
@@ -55,18 +55,18 @@
 //                             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="e.g FEMALE"/>
 //                     </div>
 
-                  
-                    
+
+
 //                     </div>
 //                     <div className=" w-100"  >
 //                     <button className='w-100 text-light btn btn-primary bg-opacity-75 fs-4 p-3'>Submit</button>
 //                    </div>
-                    
+
 //                 </div>
 
 
 //        </form>
-     
+
 //     </div>
 //   )
 // }
@@ -80,41 +80,41 @@ import Header from './Header'
 const Form = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <input type="text" />
       <div className='my-4 flex justify-center items-center'>
-         <form action="">
-                <div>
-                <h1>STUDENT FORM</h1>
-                </div>
-                <div>
-                     <div className='flex flex-col'>
-                        <label class="form-label">Name (Nom)</label>
-                        <input type="text"  className=' border border-[#7e7d7d] rounded-sm'  placeholder="e.g Adewale Philips"/>
-                    </div>
-                     <div className='flex flex-col'>
-                        <label class="form-label">Matric No (No Matricule): </label>
-                        <input type="text"  className=' border border-[#7e7d7d] rounded-sm'  placeholder="e.g Adewale Philips"/>
-                    </div>
-                     <div className='flex flex-col'>
-                        <label class="form-label">Name (Nom)</label>
-                        <input type="text"  className=' border border-[#7e7d7d] rounded-sm'  placeholder="e.g Adewale Philips"/>
-                    </div>
-                     <div className='flex flex-col'>
-                        <label class="form-label">Name (Nom)</label>
-                        <input type="text"  className=' border border-[#7e7d7d] rounded-sm'  placeholder="e.g Adewale Philips"/>
-                    </div>
-                     <div className='flex flex-col'>
-                        <label class="form-label">Name (Nom)</label>
-                        <input type="text"  className=' border border-[#7e7d7d] rounded-sm'  placeholder="e.g Adewale Philips"/>
-                    </div>
-                  
-                </div>
-                
+        <form action="">
+          <div>
+            <h1>STUDENT FORM</h1>
+          </div>
+          <div>
+            <div className='flex flex-col'>
+              <label className="form-label">Name (Nom)</label>
+              <input type="text" className=' border border-[#7e7d7d] rounded-sm' placeholder="e.g Adewale Philips" />
+            </div>
+            <div className='flex flex-col'>
+              <label className="form-label">Matric No (No Matricule): </label>
+              <input type="text" className=' border border-[#7e7d7d] rounded-sm' placeholder="e.g Adewale Philips" />
+            </div>
+            <div className='flex flex-col'>
+              <label class="form-label">Name (Nom)</label>
+              <input type="text" className=' border border-[#7e7d7d] rounded-sm' placeholder="e.g Adewale Philips" />
+            </div>
+            <div className='flex flex-col'>
+              <label className="form-label">Name (Nom)</label>
+              <input type="text" className=' border border-[#7e7d7d] rounded-sm' placeholder="e.g Adewale Philips" />
+            </div>
+            <div className='flex flex-col'>
+              <label className="form-label">Name (Nom)</label>
+              <input type="text" className=' border border-[#7e7d7d] rounded-sm' placeholder="e.g Adewale Philips" />
+            </div>
 
-               
-                </form>
-                
+          </div>
+
+
+
+        </form>
+
       </div>
     </div>
 
