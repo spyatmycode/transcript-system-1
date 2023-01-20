9c0157ad814b8029d2dc739f37112f9d32a1e71d
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-01-20 13:20:47 +0100

    worked on the ui Transcript

--- src/App.js
+++ src/App.js
@@ -4,25 +4,15 @@ import Login from './Component/Login';
 
 import { auth } from './firebase/firebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import Transcript from './Component/Transcript';
 function App() {
 
-  createUserWithEmailAndPassword(auth, "akejunifemi11@gmail.com", "123456")
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
 
   return (
     <div className="App">
-
-      <Form />
+      <Transcript/>
+      {/* <Form /> */}
+  
 
     </div>
   );
