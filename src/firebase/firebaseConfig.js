fffb466eb48e5d93a4368f4e209147cb5bd81c92
spyatmycode <akejunifemi11@gmail.com>
2023-02-03 15:36:42 +0100

    Update in Package Log JSON

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,11 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
-/* TODO: Add SDKs for Firebase products that you want to use
-https://firebase.google.com/docs/web/setup#available-libraries
 
-Your web app's Firebase configuration
-For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyByEdy32ZMhMezm4YgkQoam3FaCN2bnUF4",
     authDomain: "transcript-system-c5497.firebaseapp.com",
@@ -17,15 +13,15 @@ const firebaseConfig = {
     measurementId: "G-87ZF0NMCDC"
 };
 
-Your web app's Firebase configuration */
-const firebaseConfig = {
+
+/* const firebaseConfig = {
     apiKey: "AIzaSyDeJnF_pX2eq6vrHtlo3Dk4F6WrIMSHRxI",
     authDomain: "final-c6a63.firebaseapp.com",
     projectId: "final-c6a63",
     storageBucket: "final-c6a63.appspot.com",
     messagingSenderId: "618261984698",
     appId: "1:618261984698:web:37001e558db11763405983"
-};
+}; */
 
 
 // Initialize Firebase
