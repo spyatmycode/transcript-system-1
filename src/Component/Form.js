84bbec5e96b7caec5892a04368e805baa1070770
spyatmycode <akejunifemi11@gmail.com>
2023-04-21 18:45:46 +0100

    style(Form.js): select options for admin

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -19,10 +19,10 @@ const Form = () => {
   const {
     department,
     setDepartment,
-     localStorageDb, setLocalStorageDb,
-     setShowLocalTables,
-     showLocalTables
-    
+    localStorageDb, setLocalStorageDb,
+    setShowLocalTables,
+    showLocalTables
+
   } = useContext(AppContext)
 
   const navigate = useNavigate()
@@ -34,7 +34,7 @@ const Form = () => {
   const handleSubmit = async (e) => {
 
     e.preventDefault()
-   
+
     if (window.confirm("Are you sure of this User Information")) {
       const docref = await addDoc(transcriptHeaderCollectionRef, {
         name,
@@ -50,7 +50,7 @@ const Form = () => {
 
       console.log(transcriptHeaderCollectionRef.id);
       console.log(name);
-     
+
       //  updateDoc()
       setName("")
       setMatric("")
@@ -60,7 +60,7 @@ const Form = () => {
       setSession("")
       setLevel("")
       setShowLocalTables(false)
-     
+
 
       setLocalStorageDb([...localStorageDb, {
         name,
@@ -68,25 +68,25 @@ const Form = () => {
         college,
         // idParam:docref.id,
         id,
-        department: department === 0? "Health Information Management":
-        department === 1?" Accounting":
-        department===2?"CONFLICT RESOLUTION AND HOSPITALITY MANAGEMENT":
-        department===3? " Business":
-        department===4? "Political Science":
-        department===5? "Computer Science":
-        department===6? "Human Resources Management":
-        department===7? "Economics":
-        department===8? "Mass Communication": null
+        department: department === 0 ? "Health Information Management" :
+          department === 1 ? " Accounting" :
+            department === 2 ? "CONFLICT RESOLUTION AND HOSPITALITY MANAGEMENT" :
+              department === 3 ? " Business" :
+                department === 4 ? "Political Science" :
+                  department === 5 ? "Computer Science" :
+                    department === 6 ? "Human Resources Management" :
+                      department === 7 ? "Economics" :
+                        department === 8 ? "Mass Communication" : null
         ,
-        results: [ ]
-      
-        }])
-        // {showLocalTables ? }
-        setShowLocalTables(false)
-        showLocalTables? navigate(`/transcript/${docref.id}`):
+        results: []
+
+      }])
+      // {showLocalTables ? }
+      setShowLocalTables(false)
+      showLocalTables ? navigate(`/transcript/${docref.id}`) :
         navigate(`/transcript/${matric}`)
-        // navigate(`/transcript/${matric}`)
-  
+      // navigate(`/transcript/${matric}`)
+
 
     }
 
@@ -116,46 +116,87 @@ const Form = () => {
             <div className='flex flex-col'>
               <label className="form-label">Name (Nom)</label>
 
-              <input type="text" required value={name} onChange={(e) => setName(e.target.value)} className=' border border-[#7e7d7d] rounded-sm p-4 '  placeholder="e.g Adewale Philips" />
+              <input type="text" required value={name} onChange={(e) => setName(e.target.value)} className=' border border-[#7e7d7d] rounded-sm p-4 ' placeholder="e.g Adewale Philips" />
             </div>
             <div className='flex flex-col'>
               <label className="form-label">Matric No (No Matricule): </label>
               <input type="text" value={matric} onChange={(e) => setMatric(e.target.value)} className=' border border-[#7e7d7d] rounded-sm p-4' required placeholder="e.g TUMST-18-00015-ECN-COLSMAS" />
             </div>
-            <div className='flex flex-col'>
+
+
+
+            {/*  <div className='flex flex-col'>
               <label className="form-label">Faculty</label>
               <input type="text" value={college} onChange={(e) => setCollege(e.target.value)} className="form-control  border border-[#7e7d7d] rounded-sm p-4" required id="exampleFormControlInput1" placeholder="e.g SOCIAL & MANAGEMENT SCIENCE" />
-            </div>
-           
+            </div> */}
+
             <div className='flex flex-col'>
-              <label className="form-label">Gender</label>
-              <input value={gender} onChange={(e) => setGender(e.target.value)} type="text" className="form-control  border border-[#7e7d7d] rounded-sm p-4" required id="exampleFormControlInput1" placeholder="e.g FEMALE" />
-            </div>
-            <div className='flex flex-col'>
-              <label className="form-label">Session</label>
+              <label className="form-label">Session:</label>
               <input value={session} onChange={(e) => setSession(e.target.value)} type="text" className="form-control  border border-[#7e7d7d] rounded-sm p-4" required id="exampleFormControlInput1" placeholder="e.g 2020/2021" />
 
             </div>
+
             <div className='flex flex-col'>
               <label className="form-label"></label>
-              <ul>
+              <ul className='flex flex-col'>
 
-                    <label className="form-label"  >select your Department: </label>
-                            <select onChange={(e)=>setDepartment(Number(e.target.value))} value={Number(department)}  className='border-[1.9px] p-[.9px] border-red-900' name="" id="">
-                           
-                                <option  value={0}>Health Information Management</option>
-                                <option  value={1}>Accounting</option>
-                                <option value={2} >CONFLICT RESOLUTION AND HOSPITALITY MANAGEMENT</option>
-                                <option value={3} >Business </option>
-                                <option value={4} >Political Science</option>
-                                <option value={5} >Computer Science</option>
-                                <option value={6} >Human Resources Management</option>
-                                <option value={7} >Economics</option>
-                                <option value={8} >Mass Communication
- </option>
-                            </select>
+                <label className="form-label"  >Gender:</label>
+                <select onChange={(e) => setGender(e.target.value)} value={gender} className=' border border-[#7e7d7d] rounded-sm p-4' name="" id="">
 
-                    </ul>
+                  <option value={"Male"}>Male</option>
+
+                  <option value={"Female"}>Female</option>
+
+
+                </select>
+
+              </ul>
+            </div>
+
+            {/* <div className='flex flex-col'>
+              <label className="form-label">Gender</label>
+              <input value={gender} onChange={(e) => setGender(e.target.value)} type="text" className="form-control  border border-[#7e7d7d] rounded-sm p-4" required id="exampleFormControlInput1" placeholder="e.g FEMALE" />
+            </div> */}
+
+
+            <div className='flex flex-col'>
+             
+              <ul className='flex flex-col'>
+
+                <label className="form-label"  >Faculty:</label>
+                <select onChange={(e) => setCollege(e.target.value)} value={college} className=' border border-[#7e7d7d] rounded-sm p-4' name="" id="">
+
+                  <option value={"Science and Technology"}>Science and Technology</option>
+
+                  <option value={"Management Science"}>Management Science</option>
+                  <option value={"Arts and Communication"}>Arts and Communication</option>
+
+                  <option value={"Political and Social Science"}>Political and Social Science</option>
+
+                </select>
+
+              </ul>
+            </div>
+            <div className='flex flex-col'>
+             
+              <ul className='flex flex-col'>
+
+                <label className="form-label"  >Department:</label>
+                <select onChange={(e) => setDepartment(Number(e.target.value))} value={Number(department)} className=' border border-[#7e7d7d] rounded-sm p-4' name="" id="">
+
+                  <option value={0}>Health Information Management</option>
+                  <option value={1}>Accounting</option>
+                  <option value={2} >CONFLICT RESOLUTION AND HOSPITALITY MANAGEMENT</option>
+                  <option value={3} >Business </option>
+                  <option value={4} >Political Science</option>
+                  <option value={5} >Computer Science</option>
+                  <option value={6} >Human Resources Management</option>
+                  <option value={7} >Economics</option>
+                  <option value={8} >Mass Communication
+                  </option>
+                </select>
+
+              </ul>
             </div>
             {/* <div className='flex flex-col'>
               <label className="form-label">Level</label>
