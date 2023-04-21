f6fab42dec44586dfaf7cc6adc5a45518e8d646a
spyatmycode <akejunifemi11@gmail.com>
2023-04-21 18:59:56 +0100

    style(DeleteModal.js): background white/opacity 0.2

--- src/Component/Table/DeleteModal.js
+++ src/Component/Table/DeleteModal.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const DeleteModal = ({ message, each, onCancel, onYes , target}) => {
     return (
-        <div className="fixed inset-0 bg-opacity-40 flex items-center justify-center backdrop-blur-[2px] ">
-            <div className="bg-white rounded-lg p-8 shadow-lg border-0 border-gray-200">
+        <div className="fixed inset-0 bg-opacity-20 bg-gray-100 flex items-center justify-center backdrop-blur-[2px] ">
+            <div className="bg-white rounded-lg p-8 shadow-lg border-0 border-gray-200 ">
                 <p className="text-lg text-red-700 mb-4">{message}</p>
                 <div className="flex justify-center">
 
