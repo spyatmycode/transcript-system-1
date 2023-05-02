02b611fd1acc83f61c2ff1a53fce58e66c3149c6
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-05-02 13:35:02 +0100

    Refactor:(All js files)

--- src/Component/Table/DeleteModal.js
+++ src/Component/Table/DeleteModal.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const DeleteModal = ({ message, each, onCancel, onYes , target}) => {
     return (
-        <div className="fixed inset-0 bg-[#f12b2b0c] bg-opacity-60 flex items-center justify-center backdrop-blur-[2px] ">
+        <div className="fixed inset-0 bg-[#1b0d0d46] bg-opacity-60 flex items-center justify-center backdrop-blur-[2px] ">
             <div className="bg-[white] rounded-lg p-8 shadow-lg border-0 border-gray-200">
-                <p className="text-lg text-red-700 mb-4">{message}</p>
+                <p className="text-lg text-red-900 mb-4">{message}</p>
                 <div className="flex justify-center">
 
 
